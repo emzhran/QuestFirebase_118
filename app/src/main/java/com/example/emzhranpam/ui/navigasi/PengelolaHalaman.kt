0e2ff1d8f7a8e979d732378e581fd81c40a1167f
emzhran <zachran750@gmail.com>
2025-01-12 00:40:33 +0700

    menambahkan navigasi untuk HomeScreen

--- app/src/main/java/com/example/emzhranpam/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/emzhranpam/ui/navigasi/PengelolaHalaman.kt
@@ -3,10 +3,27 @@ package com.example.emzhranpam.ui.navigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.emzhranpam.ui.view.HomeScreen
 
 @Composable
 fun PengelolaHalaman(
     modifier: Modifier,
     navController: NavHostController = rememberNavController()
-){}
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
