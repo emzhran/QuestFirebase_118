6eb6e4107f89700ab0a09163bedd16d1b27d2bbf
emzhran <zachran750@gmail.com>
2025-01-12 00:36:23 +0700

    membuat PengelolaHalaman

---
+++ app/src/main/java/com/example/emzhranpam/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,12 @@
+package com.example.emzhranpam.ui.navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){}
