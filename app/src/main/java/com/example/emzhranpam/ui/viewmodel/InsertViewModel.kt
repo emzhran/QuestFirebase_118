85cb165db74b097634c42933b90ba236ad48df4c
emzhran <zachran750@gmail.com>
2025-01-11 23:08:27 +0700

    buat InsertViewModel dan menambahkan FormErrorState serta MahasiswaEvent

---
+++ app/src/main/java/com/example/emzhranpam/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.emzhranpam.ui.viewmodel
+
+import com.example.emzhranpam.model.Mahasiswa
+
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jeniskelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+){
+    fun isValid():Boolean{
+        return nim == null && nama == null && jeniskelamin == null
+                && alamat == null && kelas == null && angkatan == null
+    }
+}
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jeniskelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jeniskelamin = jeniskelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
