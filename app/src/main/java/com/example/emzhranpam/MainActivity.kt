771e69d4df23e1e70698ebd6fce0eb92f29036c2
emzhran <zachran750@gmail.com>
2025-01-12 02:30:44 +0700

    memanggil pengelolahalaman pada MainActivity

--- app/src/main/java/com/example/emzhranpam/MainActivity.kt
+++ app/src/main/java/com/example/emzhranpam/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.emzhranpam.ui.navigasi.PengelolaHalaman
 import com.example.emzhranpam.ui.theme.EmzhranPAMTheme
 import com.example.emzhranpam.ui.view.HomeScreen
 
@@ -20,11 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EmzhranPAMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(
-                        navigateToItemEntry = {}, modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PengelolaHalaman(modifier = Modifier)
             }
         }
     }
